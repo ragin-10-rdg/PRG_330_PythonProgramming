007adee821b6746b9785bf3cc3c0a7bc14ea8fbc
Ragin Karki <zeroremorsenepal@gmail.com>
2024-11-05 09:02:17 +0545

    Module

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import maths_utils
+import Mathematics
+from Mathematics import trigonometry
+import Mathematics.trigonometry
+cosine_of_45= Mathematics.trigonometry.cosine(45)
+trigonometry.cosine(45)
+print(cosine_of_45)
